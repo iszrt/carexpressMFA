6faae3939fbdb282181d8343a17ee100a1bacc80
ViaPreciado <preciado.vianicole@ustp.edu.ph>
2025-04-10 02:23:10 +0800

    Random

--- App.tsx
+++ App.tsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import LoginScreen from './components/LoginScreen'
-import DoctorLogin from './components/DoctorLogin'
-import PatientLogin from './components/PaitentLogin'
-//import DoctorDashboard from './components/DoctorDashboard' // Assume you have this
-//import PatientDashboard from './components/PatientDashboard' // Assume you have this
+Import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './components/LoginScreen'; // Regular login screen
+import DoctorLogin from './components/DoctorLogin'; // Doctor login screen
+import PatientLogin from './components/PatientLogin'; // Patient login screen
+import SignUp from './components/SignUp'; // Sign-up screen where users choose between doctor or patient
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="SignUp">
+        <Stack.Screen name="SignUp" component={SignUp} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="DoctorLogin" component={DoctorLogin} />
         <Stack.Screen name="PatientLogin" component={PatientLogin} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
+
+
+//Via Nicole Preciado
